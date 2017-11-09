5284b577a4df27a839e279856198a95d270f4a4e
alu0100888102 <alu0100888102@ull.edu.es>
2017-11-09 16:44:06 +0000

    Arreglando Clipping

--- Assets/GameAssets/Scripts/PlayerMovement.cs
+++ Assets/GameAssets/Scripts/PlayerMovement.cs
@@ -4,22 +4,26 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-    public float speed = 1.0f;
-    public int jumps = 1;
-    public int jumpForce = 500;
-    public int dashForce = 500;
-    public int pushdownForce = 50;
+    public float speed = 1.0f; //Velocidad de movimento del jugador
+    public int jumps = 1;   //Numero de saltos máximos sin tocar el suelo
+    public int jumpForce = 500; //Fuerza  que recibe el jugador al saltar
+    public int dashForce = 500; //Fuerza  que recibe el jugador al hacer el dash
+    public int pushdownForce = 50; //Fuerza  que recibe el jugador al bajar de una plataforma
 
-    private int jumpsleft = 1;
-    private bool dash = true;
-    private Collider2D ignoring = null;
-    private Animator anim;
-    private int ground;
-    private Rigidbody2D body;
-    private bool facingRight = false;
+    private int jumpsleft = 1;  //Saltos que le quedan al jugador
+    private bool dash = true;   //Si ha realizado el dash
+    private Collider2D ignoring = null; //Plataforma que podemos atravesar
+    private Animator anim;  //Animator del player
+    private int ground; //Máscara de capa de la capa "terrain", necesaria para algunas cosillas
+    private Rigidbody2D body;   //Componente Rigidbody2d del player
+    private bool facingRight = false;   //Si esta hacia la izquierda o la derecha
+
+    private int points = 0;
 
 	// Use this for initialization
 	void Start () {
+        //Inicializamos ciertas variables
+
         body = GetComponent<Rigidbody2D>();
         ground = 1 << LayerMask.NameToLayer("Terrain");
         anim = GetComponent<Animator>();
@@ -38,40 +42,48 @@ public class PlayerMovement : MonoBehaviour {
         }
 
 
+        //Se mueve en función del valor del eje lateral de movimiento. Luego se gira hacia la izquierda o la derecha y actualiza la condición del animador
         var move = new Vector3(Input.GetAxis("Horizontal") * speed, 0, 0);
-        if (move.x > 0.1f && !facingRight) {
-            transform.Rotate(0, 180, 0);
-            facingRight = true;
-        }
-        if (move.x < -0.1f && facingRight)
-        {
-            transform.Rotate(0, 180, 0);
-            facingRight = false;
-        }
-
-        anim.SetFloat("lateralMovement", Mathf.Abs( move.x));
+        walk(move);
 
         if (Input.GetButtonDown("Dash") && dash)
         {
-           body.AddForce(new Vector2(Input.GetAxis("Horizontal"), 0) * dashForce);
+            makeDash();
+        }
+        if (Input.GetButton("Attack"))
+        {
+            anim.SetTrigger("Attack");
         }
 
-        transform.position += move * Time.deltaTime;
+        //Se mueve y comprueba el moviento vertical
         anim.SetFloat("verticalMovement", body.velocity.y);
     }
 
+    //Función cuando choca con algo
     private void OnCollisionEnter2D (Collision2D other)
     {
-        if (other.gameObject.layer == 8)
+        if (other.gameObject.layer == 8) //la capa 8 es la del suelo
         {
             hitGround(other);
         }
     }
 
+    //funcion de hacer daño con la espada
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Enemy" && anim.GetCurrentAnimatorStateInfo(0).IsTag("attack"))
+        {
+            points++;
+            print(points);
+        }
+    }
+
+    //Función para hacer descender la plataforma en la que está.
     private void descendPlatform()
     {
-        Collider2D temp = Physics2D.OverlapCircle(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - 2f), 0.5f, ground);
-        print(temp);
+        //Si hay una plataforma cerca de sus pies, la ignora a la hora de hacer colisiones. A demás se aplica una fuerza hacia abajo
+
+        Collider2D temp = Physics2D.OverlapCircle(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - 1.6f), 0.5f, ground);
         if (temp != null && temp.gameObject.tag == "Platform")
         {
             anim.SetBool("onGround", false);
@@ -83,8 +95,12 @@ public class PlayerMovement : MonoBehaviour {
     }
 
     private void jump() {
+
+        //Si no le quedan saltos, no salta
         if (jumpsleft <= 0)
             return;
+
+        //Actualiza las variables y deja de ignorar la plataforma que estuviera ignorando. Luego aplica una fuerza hacia arriba y actualiza el animador
 
         anim.SetBool("onGround", false);
         jumpsleft--;
@@ -101,13 +117,15 @@ public class PlayerMovement : MonoBehaviour {
     }
 
     private void hitGround(Collision2D other) {
+        //Si está ignorando algo, deja de inorarlo, luego comprueba si está sobre el suelo y actualiza las variables.
+
         if (ignoring != null)
         {
             Physics2D.IgnoreCollision(ignoring.GetComponent<Collider2D>(), GetComponent<Collider2D>(), false);
             ignoring = null;
         }
-        Collider2D temp = Physics2D.OverlapCircle(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - 2f), 0.5f, ground);
-        if (temp != null)
+        Collider2D temp = Physics2D.OverlapCircle(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - 1.6f), 0.2f, ground);
+        if (temp != null) //De esta forma evitamos que se reseteen lo salto al chocar de frente contra las paredes en el aire.
         {
             anim.SetBool("onGround", true);
             jumpsleft = jumps;
@@ -115,5 +133,48 @@ public class PlayerMovement : MonoBehaviour {
             anim.SetFloat("verticalMovement", body.velocity.y);
         }
     }
+    
+    //Esta hay que mejorarla
+    private void makeDash()
+    {
+        body.AddForce(new Vector2(Input.GetAxis("Horizontal"), 0) * dashForce);
+    }
 
+    private  void walk(Vector3 movement)
+    {
+        if (movement.x > 0.1f && !facingRight)
+        {
+            transform.Rotate(0, 180, 0);
+            facingRight = true;
+        }
+        if (movement.x < -0.1f && facingRight)
+        {
+            transform.Rotate(0, 180, 0);
+            facingRight = false;
+        }
+
+        Vector3 temp = transform.position + movement * Time.deltaTime;
+        float xdist = 0.21f;
+        float yhead = 0.4f;
+        float yfoot = -1.1f;
+        Collider2D head;
+        Collider2D body;
+        Collider2D foot;
+        if (facingRight) {
+            head = Physics2D.OverlapCircle(new Vector2(gameObject.transform.position.x + xdist, gameObject.transform.position.y + yhead), 0.1f, ground);
+            body = Physics2D.OverlapCircle(new Vector2(gameObject.transform.position.x + xdist, gameObject.transform.position.y), 0.1f, ground);
+            foot = Physics2D.OverlapCircle(new Vector2(gameObject.transform.position.x + xdist, gameObject.transform.position.y + yfoot), 0.1f, ground);
+        }
+        else
+        {
+            head = Physics2D.OverlapCircle(new Vector2(gameObject.transform.position.x - xdist, gameObject.transform.position.y + yhead), 0.1f, ground);
+            body = Physics2D.OverlapCircle(new Vector2(gameObject.transform.position.x - xdist, gameObject.transform.position.y), 0.1f, ground);
+            foot = Physics2D.OverlapCircle(new Vector2(gameObject.transform.position.x - xdist, gameObject.transform.position.y + yfoot), 0.1f, ground);
+        }
+        if ((foot == null || foot.tag != "Ground") && (head == null || head.tag != "Ground") && (body == null || body.tag != "Ground"))
+            transform.position += movement * Time.deltaTime;
+
+
+        anim.SetFloat("lateralMovement", Mathf.Abs(movement.x));
+    }
 }
