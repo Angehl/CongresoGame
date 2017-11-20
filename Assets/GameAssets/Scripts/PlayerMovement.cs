6eaa5c1ea18376a98ec919931bfd9930f78e02fc
alu0100888102 <alu0100888102@ull.edu.es>
2017-11-20 12:38:04 +0000

    Mejoradas las animaciones

--- Assets/GameAssets/Scripts/PlayerMovement.cs
+++ Assets/GameAssets/Scripts/PlayerMovement.cs
@@ -8,8 +8,23 @@ public class PlayerMovement : MonoBehaviour {
     public float speed = 1.0f; //Velocidad de movimento del jugador
     public int jumps = 1;   //Numero de saltos máximos sin tocar el suelo
     public int jumpForce = 500; //Fuerza  que recibe el jugador al saltar
-    public int dashForce = 500; //Fuerza  que recibe el jugador al hacer el dash
+    public float dashSpeed = -1f; //Fuerza  que recibe el jugador al hacer el dash
     public int pushdownForce = 50; //Fuerza  que recibe el jugador al bajar de una plataforma
+
+    private int damage = 10;
+    private int type = 0;
+    public string horizontalAxis;
+    public string verticalAxis;
+    public string jumpButton;
+    public string attackButton;
+    public string dashkButton;
+    public string other;
+
+    private bool attacking = false; //Indica si está atacando
+    private bool locked = false;    //Indica si está bloqueado en la animación. Si está bloqueado no recibe inputs
+    private bool inmune = false;    //Indica si es inmune al daño
+    private bool dashing = false;
+    private bool midair = false;    //Midair indica si está suspendido en el aire
 
     private int jumpsleft = 1;  //Saltos que le quedan al jugador
     private bool dash = true;   //Si ha realizado el dash
@@ -19,65 +34,99 @@ public class PlayerMovement : MonoBehaviour {
     private Rigidbody2D body;   //Componente Rigidbody2d del player
     private bool facingRight = false;   //Si esta hacia la izquierda o la derecha
 
-    private int points = 0;
+    public int combo = 0;
+    public int points = 0;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start() {
         //Inicializamos ciertas variables
 
         body = GetComponent<Rigidbody2D>();
         ground = 1 << LayerMask.NameToLayer("Terrain");
         anim = GetComponent<Animator>();
+        //  Physics2D.IgnoreCollision(GameObject.FindGameObjectsWithTag(other)[0].GetComponent<Collider2D>(), GetComponent<Collider2D>());
     }
+
+
 
     // Update is called once per frame
     void Update() {
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown(jumpButton) && !locked)
         {
             jump();
         }
-        if (Input.GetButtonDown("SlideDown"))
-        {
-            descendPlatform();
-        }
 
+        if (onGround())
+        {
+            anim.SetBool("onGround", true);
+            dash = true;
+        }
+        else
+            anim.SetBool("onGround", false);
 
         //Se mueve en función del valor del eje lateral de movimiento. Luego se gira hacia la izquierda o la derecha y actualiza la condición del animador
-        var move = new Vector3(Input.GetAxis("Horizontal") * speed, 0, 0);
-        walk(move);
-
-        if (Input.GetButtonDown("Dash") && dash)
+        if (!locked)
         {
-            makeDash();
+            var move = new Vector3(Input.GetAxis(horizontalAxis) * speed, 0, 0);
+            walk(move);
         }
-        if (Input.GetButton("Attack"))
+
+        if (dashing)
+        {
+            var move = new Vector3(dashSpeed, 0, 0);
+            walk(move);
+        }
+
+        if (Input.GetButtonDown(dashkButton) && dash)
+            {
+                dash = false;
+                anim.SetTrigger("Dash");
+            }
+
+        if (Input.GetButtonDown(attackButton))
         {
             anim.SetTrigger("Attack");
+        }
+
+        if (midair)
+        {
+            body.velocity = Vector2.zero;
         }
 
         //Se mueve y comprueba el moviento vertical
         anim.SetFloat("verticalMovement", body.velocity.y);
     }
 
+
+
     //Función cuando choca con algo
-    private void OnCollisionEnter2D (Collision2D other)
+    private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.layer == 8) //la capa 8 es la del suelo
         {
-            hitGround(other);
+            if (onGround())
+            {
+                anim.SetBool("onGround", true);
+                jumpsleft = jumps;
+                dash = true;
+                anim.SetFloat("verticalMovement", body.velocity.y);
+            }
         }
     }
+
 
     //funcion de hacer daño con la espada
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.tag == "Enemy" && anim.GetCurrentAnimatorStateInfo(0).IsTag("attack"))
+        if (other.gameObject.layer == 9 && other.gameObject.tag != gameObject.tag && attacking)
         {
-            points++;
-            print(points);
+            if (other.gameObject.name == "Sword")
+                return;
+            points += other.gameObject.GetComponent<PlayerMovement>().hit(damage, type) * (1 + combo);
         }
     }
+
 
     //Función para hacer descender la plataforma en la que está.
     private void descendPlatform()
@@ -91,11 +140,17 @@ public class PlayerMovement : MonoBehaviour {
             ignoring = temp;
             Physics2D.IgnoreCollision(ignoring.GetComponent<Collider2D>(), GetComponent<Collider2D>());
             body.AddForce(Vector2.down * pushdownForce);
-            anim.SetFloat("verticalMovement", body.velocity.y);
         }
     }
 
+
     private void jump() {
+
+        if (Input.GetAxis(verticalAxis) < -0.1)
+        {
+            descendPlatform();
+            return;
+        }
 
         //Si no le quedan saltos, no salta
         if (jumpsleft <= 0)
@@ -103,7 +158,7 @@ public class PlayerMovement : MonoBehaviour {
 
         //Actualiza las variables y deja de ignorar la plataforma que estuviera ignorando. Luego aplica una fuerza hacia arriba y actualiza el animador
 
-        anim.SetBool("onGround", false);
+        anim.SetTrigger("Jump");
         jumpsleft--;
 
         if (ignoring != null)
@@ -112,12 +167,12 @@ public class PlayerMovement : MonoBehaviour {
             ignoring = null;
         }
 
-        body.velocity = new Vector2(0, 0);
+        body.velocity = Vector2.zero;
         body.AddForce(Vector2.up * jumpForce);
-        anim.SetFloat("verticalMovement", body.velocity.y);
     }
 
-    private void hitGround(Collision2D other) {
+    //Devuelve si está o no tocando el suelo
+    private bool onGround() {
         //Si está ignorando algo, deja de inorarlo, luego comprueba si está sobre el suelo y actualiza las variables.
 
         if (ignoring != null)
@@ -128,20 +183,35 @@ public class PlayerMovement : MonoBehaviour {
         Collider2D temp = Physics2D.OverlapCircle(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - 1.6f), 0.2f, ground);
         if (temp != null) //De esta forma evitamos que se reseteen lo salto al chocar de frente contra las paredes en el aire.
         {
-            anim.SetBool("onGround", true);
-            jumpsleft = jumps;
-            dash = true;
-            anim.SetFloat("verticalMovement", body.velocity.y);
+            return true;
         }
-    }
-    
-    //Esta hay que mejorarla
-    private void makeDash()
-    {
-        body.AddForce(new Vector2(Input.GetAxis("Horizontal"), 0) * dashForce);
+        return false;
     }
 
-    private  void walk(Vector3 movement)
+
+    //Comienza el dash
+    private void makeDash()
+    {
+        locked = true;
+        midair = true;
+    }
+
+    private void dashMovement()
+    {
+        if ((dashSpeed < 0 && facingRight) || (dashSpeed > 0 && !facingRight))
+            dashSpeed *= -1;
+
+        dashing = !dashing;
+        inmune = !inmune;
+    }
+
+    private void endDash()
+    {
+        locked = false;
+        midair = false;
+    }
+
+    private void walk(Vector3 movement)
     {
         if (movement.x > 0.1f && !facingRight)
         {
@@ -174,8 +244,42 @@ public class PlayerMovement : MonoBehaviour {
         }
         if ((foot == null || foot.tag != "Ground") && (head == null || head.tag != "Ground") && (body == null || body.tag != "Ground"))
             transform.position += movement * Time.deltaTime;
-
-
         anim.SetFloat("lateralMovement", Mathf.Abs(movement.x));
+    }
+
+
+    public int hit(int damage, int type)
+    {
+        if (inmune)
+            return 0;
+
+        anim.SetTrigger("damaged");
+        combo = 0;
+
+        if (attacking)
+            damage *= 2;
+
+        if (type == 2)
+            damage *= 2;
+
+        return damage;
+    }
+
+    private void attack(int type)
+    {
+        attacking = !attacking;
+        locked = !locked;
+        if (!locked && type == 2)
+            anim.ResetTrigger("Attack");
+    }
+
+    private void locks()
+    {
+        locked = true;
+    }
+
+    private void unlocks()
+    {
+        locked = false;
     }
 }
